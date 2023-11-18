3c8304b3ab633bce6f6ef105afd69056f5e43341
Christine Hoang <christinehoangt@gmail.com>
2023-11-17 21:59:07 -0600

    top-fossils page back rendering now

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ import lodash from 'lodash';
 import morgan from 'morgan';
 import nunjucks from 'nunjucks';
 import ViteExpress from 'vite-express';
-import axios from 'axios';
 
 const app = express();
 const port = '8000';
@@ -61,13 +60,7 @@ const OTHER_FOSSILS = [
   },
 ];
 
-const DEFAULT_FOSSIL = 'Broncosaurus';
 
-//FossilID from MOST LIKED FOSSILS
-
-function getFossilDetails(fossilID){
-  return MOST_LIKED_FOSSILS[fossilID];
-}
 
 ////// ROUTES //////////////////////
 app.get('/', (req, res) => {
@@ -82,13 +75,13 @@ app.get('/top-fossils', (req, res) => {
   res.render('top-fossils.html.njk');
 });
 
-app.get('/rank', (req, res) => {
-  res.render('rank.html.njk');
-});
-
 app.get('/thank-you', (req, res) => {
   res.render('thank-you.html.njk');
 });
+
+app.get('/rank', (req, res) => {
+  res.render('rank.html.njk');
+ })
 
 app.get('/random-fossil.json', (req, res) => {
   const randomFossil = lodash.sample(OTHER_FOSSILS);
@@ -104,89 +97,70 @@ app.get('/logout', (req, res) => {
 });
 });
 
-app.get('/like-fossil', (req, res) => {
-  res.render('rank.html.njk');
-});
+app.post('/like-fossil', (req, res) => {
+
+const {fossil} = req.body
+req.session.fossile = fossil
+for(const key in MOST_LIKED_FOSSILS){
+  if(key ===fossil){
+    MOST_LIKED_FOSSILS[fossil].num_likes++
+  }
+}
+res.render ('thank-you.html.njk', {name: req.session.name})
+})
+
 
 ////////////Top Fossils is like Add to Cart///////////////
 
-app.get('/top-fossils/:fossilID', (req, res) => {
-if(req.session.name){
-  let sess = req.session;
-  let {Aust,Quetz,Steg, Tyra } = MOST_LIKED_FOSSILS;
+app.get('/top-fossils', (req, res) =>{
+  if (req.session.name) {
+    res.render('top-fossils.html.njk', {
+      mostLiked: Object.values(MOST_LIKED_FOSSILS), name: req.session.name
+    });
+  } else {
+    res.redirect('top-fossils.html.njk');
+  };
+});
 
-  console.log(MOST_LIKED_FOSSILS);
-
-  if (!sess.rank){
-    sess.rank = {};
-}
-
-if (!sess.rank[fossilId]){
-  sess.rank[fossilId] = num_likes;
-}
-
-sess.rank[fossilID]++;
-console.log(sess.rank[fossilID]);
-res.redirect('/rank');
-}});
 
 
 /////////////Rank Page is the Cart/////////////////
-app.get('/rank', (req,res)=>{
-const sess = req.session;
+// app.get('/rank', (req,res)=>{
+// const sess = req.session;
 
-if(!sess.rank){
-  sess.rank = {};
-}
-console.log("/rank hit");
+// if(!sess.rank){
+//   sess.rank = {};
+// }
+// console.log("/rank hit");
 
-let rank = sess.rank;
-console.log(rank);
-let arrRank = [];
-let rankTotal = 0;
+// let rank = sess.rank;
+// console.log(rank);
+// let arrRank = [];
+// let rankTotal = 0;
 
-for (let fossilID in rank){
-  const qty = sess.rank[fossilID];
-  console.log(fossilID);
+// for (let fossilID in rank){
+//   const qty = sess.rank[fossilID];
+//   console.log(fossilID);
 
-  let fossilDetails = getFossilDetails(fossilID);
-  rankTotal += qty * fossilDetails.num_likes;
-  fossilDetails.num_likes = qty;
-  arrRank.push(fossilDetails);
+//   let fossilDetails = getFossilDetails(fossilID);
+//   rankTotal += qty * fossilDetails.num_likes;
+//   fossilDetails.num_likes = qty;
+//   arrRank.push(fossilDetails);
 
-  console.log(qty);
-  console.log(fossilDetails);
-}
-res.render('rank.html.njk', {arrRank, rankTotal});
-});
+//   console.log(qty);
+//   console.log(fossilDetails);
+// }
+// res.render('rank.html.njk', {arrRank, rankTotal});
+// });
 
 
   /////////////////////Username//////////////////////////
 
-function getName(user=username){
-  const username = 'user';
-  const newArr = [];
-  document.querySelector('#username').innerText = text;
-  if(user === username){
-    newArr.push(user);
-  }
-}
-
-  // app.get('/homepage', (req, res) => {
-  //   res.render('/homepage.html.njk');
-  // });
-
-  // app.post('/process-login', (req, res) => {
-  //   for(let user of users){
-  //     if (req.body.username === user.username){
-  //       console.log(req.body.username);
-  //       res.redirect('/top-fossils');
-  //       return;
-  //     }
-  //   }
-  //   res.render('/homepage.html.njk', { message: 'You forgot to share your name!' });
-  // });
-
+  app.post('/get-name', (req, res) => {
+    const { name } = req.body
+      req.session.name = name
+      res.redirect('/top-fossils')
+  })
 
 ViteExpress.listen(app, port, () => {
   console.log(`Server running on http://localhost:${port}...`);
